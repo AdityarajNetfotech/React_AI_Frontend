807db4bd0cb3a1ac269a74d2091756e955552a99
AdityaRaj <aditya@netfotech.in>
2025-07-21 10:45:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css' // Assuming you have an App.css for styles
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      Hi
+    </div>
+  )
+}
+
+export default App
