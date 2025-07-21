52b35d00f84d17ba6434d8629c8dcf53596eac92
AdityaRaj <aditya@netfotech.in>
2025-07-21 13:25:10 +0530

    First commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './App.css' // Assuming you have an App.css for styles
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Link,
+} from "react-router-dom";
 
+import DashboardSidebar from './Components/Recruter/DashboardSidebar';
+import Dashboard from './Components/Admin/Dashboard';
+import DashboardStudent from './Components/Candidates/DashboardStudent';
+import Register from './Components/Auth/Register';
 const App = () => {
   return (
+
+    
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      Hi
+      <Router>
+      <Routes>
+        <Route path="/DashboardSidebar" element={<DashboardSidebar />} />
+        </Routes>
+        </Router>
     </div>
   )
 }
---
+++ src/Components/Recruter/DashboardSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardSidebar() {
+  return (
+    <div>
+      Hii
+    </div>
+  )
+}
+
+export default DashboardSidebar
