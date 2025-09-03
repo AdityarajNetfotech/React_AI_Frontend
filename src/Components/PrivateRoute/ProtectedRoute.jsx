a7d3c900a2ff2faca5b10a2f5a634e517a8818f1
AdityaRaj <aditya@netfotech.in>
2025-09-03 17:17:13 +0530

    Updateed again

--- src/Components/PrivateRoute/ProtectedRoute.jsx
+++ src/Components/PrivateRoute/ProtectedRoute.jsx
@@ -4,8 +4,9 @@ import { Outlet, Navigate } from "react-router-dom";
 const ProtectedRoute = () => {
     const adminToken = localStorage.getItem("adminAuthToken");
     const recruiterToken = localStorage.getItem("recruiterAuthToken");
+    const candidateAuthToken = localStorage.getItem("candidateAuthToken");
 
-    return adminToken || recruiterToken ? (
+    return adminToken || recruiterToken || candidateAuthToken ?  (
         <Outlet />
     ) : (
         <Navigate to="/" />
